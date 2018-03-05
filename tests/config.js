f2cb6a97adf596fb4449878c2596ab56c6b6eca6
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2018-03-05 18:39:49 -0300

    Adjustign usage of webpack in new version 4

--- tests/config.js
+++ tests/config.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let path = require('path');
-let webpack = require('webpack');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = [
 	{
 		entry: './tests/fixtures/index.css',
 
 		output: {
-			path: './tests/cache',
+			path: path.join(__dirname, 'cache'),
 			filename: 'index.css'
 		},
 
@@ -21,19 +21,19 @@ module.exports = [
 			rules: [
 				{
 					test: /\.(png|css|woff)$/,
-					loader: path.join(__dirname, '../index.js'),
-					query: {
-						limit: 1000,
-						name: '[name].[ext]'
+					use: {
+						loader: path.join(__dirname, '../index.js'),
+						query: {
+							limit: 1000,
+							name: '[name].[ext]'
+						}
 					}
 				},
-
 				{
 					test: /\.css$/,
-
 					use: ExtractTextPlugin.extract({
-						fallbackLoader: 'style-loader',
-						loader: 'css-loader'
+						fallback: 'style-loader',
+						use: 'css-loader'
 					})
 				}
 			]
